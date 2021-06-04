4e530ad9416ec84888a43e5b98f0ae80b34162e2
A67645 <sharkbait@LAPTOP-DL74QR46.localdomain>
2021-06-04 01:23:56 +0100

    script dos pontos feito

--- Componente_Individual/src/Gerador_BC.py
+++ Componente_Individual/src/Gerador_BC.py
@@ -1,5 +1,33 @@
-import pandas as ps
+import pandas as pd
+import re
 
-df = pd.read_excel(r'C:\Users\thech\Desktop\MIEI\SRCR\Componente_Individual\data\dataset.xlsx')
+dataframe = pd.read_excel('../data/dataset.xlsx', engine='openpyxl')
 
-print(df)
+knowledgebase = open('base_conhecimento.pl', "w")
+
+i = 0
+
+pontos_recolha = {}
+
+while i < len(dataframe['OBJECTID']): 
+
+    id_local = str(re.sub(':.*', '',  dataframe.iloc[i]['PONTO_RECOLHA_LOCAL']))
+    nome_rua = re.sub('\\(.*', '', re.sub('[0-9]+: ', '', dataframe.iloc[i]['PONTO_RECOLHA_LOCAL']))
+    rua_anterior = re.sub('-.*', '', re.sub('.*\\): ', '', dataframe.iloc[i]['PONTO_RECOLHA_LOCAL']))
+    rua_seguinte = re.sub('\\)', '', re.sub('.*- ', '', dataframe.iloc[i]['PONTO_RECOLHA_LOCAL']))
+    latitude = str(dataframe.iloc[i]['Latitude'])
+    longitude = str(dataframe.iloc[i]['Longitude'])
+
+    ambos = "0"
+
+    if "Ambos" in dataframe.iloc[i]['PONTO_RECOLHA_LOCAL']:
+        ambos = "1"
+
+    pontos_recolha[id_local] = [id_local, latitude, longitude, nome_rua, rua_anterior, rua_seguinte, ambos]
+
+    i = i + 1
+
+for key,values in pontos_recolha.items():
+    knowledgebase.write('ponto_recolha(' + values[0] + ', ' + values[1] + ', ' + values[2] + ', "' + values[3] + '", "' + values[4] + '", "' + values[5] + '", ' + values[6] + ').\n')
+
+knowledgebase.close()
---
+++ Componente_Individual/src/base_conhecimento.pl
@@ -0,0 +1,121 @@
+ponto_recolha(15805, -9.14330880914792, 38.7080787857025, "R do Alecrim ", "R Ferragial ", "R Ataíde", 0).
+ponto_recolha(15806, -9.14337777820218, 38.7080781891571, "R do Alecrim ", "R Ataíde ", "R Ferragial", 0).
+ponto_recolha(15807, -9.14348180670535, 38.7073026157039, "R do Alecrim ", "R Ferragial ", "Pc Duque da Terceira", 0).
+ponto_recolha(15808, -9.14255098678099, 38.7073286838222, "R Corpo Santo ", "Lg Corpo Santo ", "Tv Corpo Santo", 1).
+ponto_recolha(15809, -9.14276596081499, 38.7070836135523, "Tv Corpo Santo ", "R Corpo Santo ", "R Bernardino da Costa", 1).
+ponto_recolha(15810, -9.1426225690344, 38.7066975168166, "Tv Corpo Santo ", "R Bernardino da Costa ", "Cais do Sodré", 1).
+ponto_recolha(15811, -9.14240835587344, 38.7069966274245, "R Bernardino da Costa ", "Lg Corpo Santo ", "Tv Corpo Santo", 1).
+ponto_recolha(15812, -9.14305015543156, 38.7068559589223, "R Bernardino da Costa ", "Tv Corpo Santo ", "Pc Duque da Terceira", 1).
+ponto_recolha(15813, -9.1512511235953, 38.7087754470349, "Lg Conde-Barão ", "R da Boavista ", "Bqr do Duro", 1).
+ponto_recolha(15814, -9.15178946418214, 38.7086356323308, "Lg Conde-Barão ", "Bqr do Duro ", "R Mastros", 1).
+ponto_recolha(15815, -9.15201897924565, 38.708606605818, "Lg Conde-Barão ", "R Mastros ", "Tv do Cais do Tojo", 1).
+ponto_recolha(15818, -9.14910552718357, 38.709073383915, "Tv Marquês de Sampaio ", "R da Boavista ", "R da Boavista", 1).
+ponto_recolha(15819, -9.14764976145157, 38.708563591768, "R da Boavista ", "R São Paulo ", "Tv Marquês de Sampaio", 1).
+ponto_recolha(15820, -9.1485717796828, 38.7087267228466, "R da Boavista ", "Tv Marquês de Sampaio ", "Bqr dos Ferreiros", 1).
+ponto_recolha(15821, -9.14911344583279, 38.7088210959641, "R da Boavista ", "Bqr dos Ferreiros ", "Tv Marquês de Sampaio", 1).
+ponto_recolha(15822, -9.14949354638571, 38.7088718347208, "R da Boavista ", "Tv Marquês de Sampaio ", "Pto Galega (Rua da Boavista, 118", 1).
+ponto_recolha(15823, -9.15018388371526, 38.7089108606806, "R da Boavista ", "Pto Galega (Rua da Boavista, 118) ", "R Instituto Industrial", 1).
+ponto_recolha(15824, -9.15079311664937, 38.7089055507175, "R da Boavista ", "R Instituto Industrial ", "Lg Conde-Barão", 1).
+ponto_recolha(15825, -9.15206034846112, 38.7082819634324, "Tv do Cais do Tojo ", "R Cais do Tojo ", "R Cais do Tojo", 1).
+ponto_recolha(15827, -9.15247211372946, 38.7081342433687, "R Cais do Tojo ", "Av Dom Carlos I ", "Tv do Cais do Tojo", 1).
+ponto_recolha(15828, -9.15178357572543, 38.7082213241484, "R Cais do Tojo ", "Tv do Cais do Tojo ", "Bqr do Duro", 1).
+ponto_recolha(15830, -9.15154474333579, 38.7084035643017, "Bqr do Duro ", "R Cais do Tojo ", "R Cais do Tojo", 1).
+ponto_recolha(15831, -9.1513885281045, 38.7079275125773, "Bqr do Duro ", "R Cais do Tojo ", "R Dom Luís I", 1).
+ponto_recolha(15832, -9.14751394875702, 38.7103393147562, "Tv Santa Catarina, 4-6", "15832: Tv Santa Catarina, 4", "15832: Tv Santa Catarina, 4-6", 0).
+ponto_recolha(15833, -9.14856601878688, 38.7099428303287, "R Ferreiros a Santa Catarina, 20", "15833: R Ferreiros a Santa Catarina, 20", "15833: R Ferreiros a Santa Catarina, 20", 0).
+ponto_recolha(15834, -9.15046432876378, 38.7068005850863, "R Instituto Industrial ", "Av 24 de Julho ", "R Dom Luís I", 1).
+ponto_recolha(15836, -9.1505486401448, 38.7078807891527, "R Instituto Industrial ", "R Dom Luís I ", "R da Boavista", 1).
+ponto_recolha(15838, -9.14851812420916, 38.7098081294277, "R Santa Catarina, 19", "15838: R Santa Catarina, 19", "15838: R Santa Catarina, 19", 0).
+ponto_recolha(15841, -9.14704425203433, 38.7080193757895, "R Moeda ", "R São Paulo ", "Pc Dom Luís I", 1).
+ponto_recolha(15842, -9.14614421827306, 38.707783978131, "Tv Carvalho ", "R Ribeira Nova ", "Tv Carvalho", 1).
+ponto_recolha(15843, -9.14563984658037, 38.7078874408968, "Tv Carvalho ", "Tv Carvalho ", "Pc São Paulo", 1).
+ponto_recolha(15844, -9.14598863635406, 38.7081636570213, "Tv Carvalho ", "Tv Carvalho ", "R São Paulo", 1).
+ponto_recolha(15845, -9.15199352638039, 38.7076249751805, "R Dom Luís I ", "Av Dom Carlos I ", "Bqr do Duro", 1).
+ponto_recolha(15846, -9.15093562538604, 38.7076071810499, "R Dom Luís I ", "Bqr do Duro ", "R Instituto Industrial", 1).
+ponto_recolha(15847, -9.14993558128589, 38.7076158965573, "R Dom Luís I ", "R Instituto Industrial ", "Bqr dos Ferreiros", 1).
+ponto_recolha(15848, -9.14829055489657, 38.7075401361692, "R Dom Luís I ", "Bqr dos Ferreiros ", "Pc Dom Luís I", 1).
+ponto_recolha(15849, -9.14714620183592, 38.707099690356, "Pc Dom Luís I ", "R Dom Luís I ", "Av 24 de Julho", 1).
+ponto_recolha(15850, -9.14835399212529, 38.7103950657776, "Tv Condessa do Rio, Fte 3", "15850: Tv Condessa do Rio, Fte 3", "15850: Tv Condessa do Rio, Fte 3", 0).
+ponto_recolha(15851, -9.14671590072996, 38.7075628281927, "Pc Dom Luís I ", "R Ribeira Nova ", "R Dom Luís I", 1).
+ponto_recolha(15852, -9.14493001491084, 38.7072810634258, "R Ribeira Nova ", "R Remolares ", "R Instituto Dona Amélia", 1).
+ponto_recolha(15853, -9.14585366538093, 38.7075613035211, "R Ribeira Nova ", "R Instituto Dona Amélia ", "Pc Dom Luís I", 1).
+ponto_recolha(15855, -9.14350797013798, 38.7075275840864, "R São Paulo ", "R Corpo Santo ", "R Flores", 0).
+ponto_recolha(15856, -9.14415472749814, 38.7077381729183, "R São Paulo ", "R Flores ", "Pc São Paulo", 0).
+ponto_recolha(15857, -9.14418870357883, 38.707701846559, "R São Paulo ", "Pc São Paulo ", "Tv dos Remolares", 0).
+ponto_recolha(15858, -9.14351882987146, 38.7074824516687, "R São Paulo ", "Tv dos Remolares ", "Tv Corpo Santo", 0).
+ponto_recolha(15859, -9.14475474461576, 38.7078951146912, "R São Paulo ", "Pc São Paulo ", "Pc São Paulo", 1).
+ponto_recolha(15860, -9.14552858843579, 38.7081496329634, "R São Paulo ", "Pc São Paulo ", "Tv Carvalho", 1).
+ponto_recolha(15861, -9.14606015123382, 38.7083431936003, "R São Paulo ", "Tv Carvalho ", "Bc da Moeda", 1).
+ponto_recolha(15862, -9.14634879700261, 38.7084307669213, "R São Paulo ", "Bc da Moeda ", "Cc da Bica Grande", 1).
+ponto_recolha(15863, -9.14664843006564, 38.7084822125522, "R São Paulo ", "Cc da Bica Grande ", "R Moeda", 1).
+ponto_recolha(15864, -9.14695866414731, 38.7084705102342, "R São Paulo ", "R Moeda ", "R da Boavista", 1).
+ponto_recolha(15865, -9.14428821714939, 38.7074217423076, "R Nova do Carvalho ", "Pc São Paulo ", "Tv dos Remolares", 1).
+ponto_recolha(15866, -9.14381451843206, 38.7072546959625, "R Nova do Carvalho ", "Tv dos Remolares ", "R do Alecrim", 1).
+ponto_recolha(15867, -9.14319266057494, 38.7071790073949, "R Nova do Carvalho ", "R do Alecrim ", "Tv Corpo Santo", 1).
+ponto_recolha(15868, -9.14402783597271, 38.7068925345681, "R Remolares ", "Pc Duque da Terceira ", "Tv dos Remolares", 1).
+ponto_recolha(15869, -9.14447892499263, 38.7070868003123, "R Remolares ", "Tv dos Remolares ", "Tv Ribeira Nova", 1).
+ponto_recolha(15871, -9.14441649477531, 38.7067360350326, "Tv dos Remolares ", "Av 24 de Julho ", "R Remolares", 1).
+ponto_recolha(15873, -9.14416983805955, 38.7071795562796, "Tv dos Remolares ", "R Nova do Carvalho ", "R Nova do Carvalho", 1).
+ponto_recolha(15875, -9.14401335962188, 38.7074961856438, "Tv dos Remolares ", "R São Paulo ", "R Nova do Carvalho", 1).
+ponto_recolha(15876, -9.14404732851606, 38.7058294413797, "Cais do Sodré ", "Av 24 de Julho ", "R Cintura (Santos", 1).
+ponto_recolha(15877, -9.14324233291858, 38.7058093867279, "Cais do Sodré ", "R Cintura (Santos) ", "Pc Duque da Terceira", 1).
+ponto_recolha(15878, -9.14281417070135, 38.7064256244632, "Cais do Sodré ", "Pc Duque da Terceira ", "Tv Corpo Santo", 1).
+ponto_recolha(15879, -9.14222984894838, 38.7065657946941, "Cais do Sodré ", "Tv Corpo Santo ", "Lg Corpo Santo", 1).
+ponto_recolha(15880, -9.14622006213417, 38.7082697430488, "Bc da Moeda ", "R São Paulo ", "R São Paulo", 1).
+ponto_recolha(15881, -9.14956443154647, 38.7090063348014, "Pto Galega ", "R da Boavista ", "R da Boavista", 1).
+ponto_recolha(15882, -9.15059949287472, 38.709033347926, "Bc da Boavista ", "R da Boavista ", "R da Boavista", 1).
+ponto_recolha(15883, -9.1446205470921, 38.7073467997814, "Tv Ribeira Nova ", "R Remolares ", "R Nova do Carvalho", 1).
+ponto_recolha(15884, -9.14476400355141, 38.7069221884751, "Pc Ribeira Nova ", "Av 24 de Julho ", "R Ribeira Nova", 1).
+ponto_recolha(15885, -9.14508822170713, 38.7079012322757, "Pc São Paulo ", "R São Paulo ", "Tv Carvalho", 1).
+ponto_recolha(15886, -9.14488886847759, 38.7076237175904, "Pc São Paulo ", "Tv Carvalho ", "Tv Ribeira Nova", 1).
+ponto_recolha(15887, -9.14447518350945, 38.7076363096132, "Pc São Paulo ", "Tv Ribeira Nova ", "R São Paulo", 1).
+ponto_recolha(15888, -9.14329884772974, 38.7065565483955, "Pc Duque da Terceira ", "Cais do Sodré ", "R Bernardino da Costa", 1).
+ponto_recolha(15889, -9.14340471238935, 38.7067267810448, "Pc Duque da Terceira ", "R Bernardino da Costa ", "R do Alecrim", 1).
+ponto_recolha(15890, -9.14361174262131, 38.7067339965055, "Pc Duque da Terceira ", "R do Alecrim ", "R Remolares", 1).
+ponto_recolha(15891, -9.1437590142256, 38.7065795884311, "Pc Duque da Terceira ", "R Remolares ", "Av 24 de Julho", 1).
+ponto_recolha(15892, -9.14200502758224, 38.7069280516782, "Lg Corpo Santo ", "Cais do Sodré ", "R Arsenal", 1).
+ponto_recolha(15893, -9.14201010036865, 38.7072883208907, "Lg Corpo Santo ", "R Arsenal ", "Cc do Ferragial", 1).
+ponto_recolha(15894, -9.1420813502454, 38.7074498453842, "Lg Corpo Santo ", "Cc do Ferragial ", "R Corpo Santo", 1).
+ponto_recolha(15896, -9.14986368552042, 38.7090307521821, "Bc Francisco André ", "R da Boavista ", "R da Boavista", 1).
+ponto_recolha(15897, -9.14534815013291, 38.7075837039225, "Tv de São Paulo ", "Pc São Paulo ", "R Ribeira Nova", 1).
+ponto_recolha(15898, -9.14437680001953, 38.7063670578938, "Av 24 de Julho ", "Pc Duque da Terceira ", "Tv dos Remolares", 0).
+ponto_recolha(15899, -9.14473414894124, 38.7064360246404, "Av 24 de Julho ", "Tv dos Remolares ", "Pc Ribeira Nova", 0).
+ponto_recolha(19365, -9.14751690960422, 38.7146089969724, "R O Século, 150", "19365: R O Século, 150", "19365: R O Século, 150", 0).
+ponto_recolha(19216, -9.15186340541146, 38.7154539083141, "R Marcos Portugal, Muro do Lg Agostinho da Silva", "19216: R Marcos Portugal, Muro do Lg Agostinho da Silva", "19216: R Marcos Portugal, Muro do Lg Agostinho da Silva", 0).
+ponto_recolha(19280, -9.14754956156816, 38.7112307798315, "Tv André Valente, jt silo", "19280: Tv André Valente, jt silo", "19280: Tv André Valente, jt silo", 0).
+ponto_recolha(19301, -9.14422400736949, 38.7069448834987, "R Remolares, Crzt Tv Remolares", "19301: R Remolares, Crzt Tv Remolares", "19301: R Remolares, Crzt Tv Remolares", 0).
+ponto_recolha(19315, -9.14440731848469, 38.7101590906286, "R Emenda, 100", "19315: R Emenda, 100", "19315: R Emenda, 100", 0).
+ponto_recolha(19300, -9.14490054388133, 38.7108934583781, "R Salgadeiras, SFC CZT  R. da Atalaia", "19300: R Salgadeiras, SFC CZT  R. da Atalaia", "19300: R Salgadeiras, SFC CZT  R. da Atalaia", 0).
+ponto_recolha(19407, -9.14388063353866, 38.7094970732753, "Tv Guilherme Cossoul, 10", "19407: Tv Guilherme Cossoul, 10", "19407: Tv Guilherme Cossoul, 10", 0).
+ponto_recolha(19282, -9.1513885505624, 38.7087382169296, "Lg Conde-Barão, 19", "19282: Lg Conde", "19282: Lg Conde-Barão, 19", 0).
+ponto_recolha(19257, -9.1531085611251, 38.713272150325, "R São Bento, 212", "19257: R São Bento, 212", "19257: R São Bento, 212", 0).
+ponto_recolha(19293, -9.15311830964257, 38.7163797615419, "R Imprensa Nacional, 46", "19293: R Imprensa Nacional, 46", "19293: R Imprensa Nacional, 46", 0).
+ponto_recolha(19310, -9.14226074740088, 38.7079437265862, "R Vitor Cordon, 43", "19310: R Vitor Cordon, 43", "19310: R Vitor Cordon, 43", 0).
+ponto_recolha(19319, -9.14481349084208, 38.7079846835909, "R São Paulo, 108", "19319: R São Paulo, 108", "19319: R São Paulo, 108", 0).
+ponto_recolha(19236, -9.15360843944475, 38.7144658214995, "R São Bento, 318", "19236: R São Bento, 318", "19236: R São Bento, 318", 0).
+ponto_recolha(19295, -9.14460348898318, 38.7142829757734, "R Teixeira, 1", "19295: R Teixeira, 1", "19295: R Teixeira, 1", 0).
+ponto_recolha(19371, -9.14464751990681, 38.7108866428877, "R Salgadeiras, SFC Jt 11", "19371: R Salgadeiras, SFC Jt 11", "19371: R Salgadeiras, SFC Jt 11", 0).
+ponto_recolha(19390, -9.15242517945081, 38.7096839983153, "R Poço dos Negros, crzt R São Bento", "19390: R Poço dos Negros, crzt R São Bento", "19390: R Poço dos Negros, crzt R São Bento", 0).
+ponto_recolha(19278, -9.15305561262562, 38.7119754868767, "R Correia Garção, 3", "19278: R Correia Garção, 3", "19278: R Correia Garção, 3", 0).
+ponto_recolha(19415, -9.14301166531656, 38.7073877536231, "R São Paulo, 14 a 16", "19415: R São Paulo, 14 a 16", "19415: R São Paulo, 14 a 16", 0).
+ponto_recolha(19337, -9.14385688118425, 38.7094432317241, "Tv Guilherme Cossoul, 11", "19337: Tv Guilherme Cossoul, 11", "19337: Tv Guilherme Cossoul, 11", 0).
+ponto_recolha(21843, -9.15264387969663, 38.7105108085557, "R São Bento, 15", "21843: R São Bento, 15", "21843: R São Bento, 15", 0).
+ponto_recolha(21844, -9.15130039259538, 38.7122430297428, "R Cruz dos Poiais de São Bento, Crt R das Parreiras", "21844: R Cruz dos Poiais de São Bento, Crt R das Parreiras", "21844: R Cruz dos Poiais de São Bento, Crt R das Parreiras", 0).
+ponto_recolha(21957, -9.14752862812709, 38.7056911463271, "R da Cintura do Porto de Lisboa, 3154 Armazém B", "21957: R da Cintura do Porto de Lisboa, 3154 Armazém B", "21957: R da Cintura do Porto de Lisboa, 3154 Armazém B", 0).
+ponto_recolha(21959, -9.1507756102811, 38.7084823352254, "R Instituto Industrial, 7-7D", "21959: R Instituto Industrial, 7", "21959: R Instituto Industrial, 7-7D", 0).
+ponto_recolha(21911, -9.14605416845402, 38.707919877341, "Tv Carvalho, frt nº 29", "21911: Tv Carvalho, frt nº 29", "21911: Tv Carvalho, frt nº 29", 0).
+ponto_recolha(21952, -9.14906797670637, 38.7088575233976, "R da Boavista, 88-94", "21952: R da Boavista, 88", "21952: R da Boavista, 88-94", 0).
+ponto_recolha(21969, -9.15158073972755, 38.7077006401431, "R Dom Luís I, 34", "21969: R Dom Luís I, 34", "21969: R Dom Luís I, 34", 0).
+ponto_recolha(21854, -9.14444160448415, 38.7093300738379, "R Emenda, 41-43-45-47", "21854: R Emenda, 41", "21854: R Emenda, 41-43-45-47", 0).
+ponto_recolha(21938, -9.14408342911238, 38.7116481877359, "R Diário de Notícias, 40-A", "21938: R Diário de Notícias, 40", "21938: R Diário de Notícias, 40-A", 0).
+ponto_recolha(21961, -9.14329248945415, 38.7077366293235, "R do Alecrim, 24", "21961: R do Alecrim, 24", "21961: R do Alecrim, 24", 0).
+ponto_recolha(21866, -9.14731529890835, 38.7117372535528, "R O Século, 12", "21866: R O Século, 12", "21866: R O Século, 12", 0).
+ponto_recolha(21966, -9.14642470554241, 38.7105469477308, "R Marechal Saldanha, 26-26/A", "21966: R Marechal Saldanha, 26", "21966: R Marechal Saldanha, 26-26/A", 0).
+ponto_recolha(21967, -9.14497783133511, 38.7106675928332, "R Loreto, 57-59", "21967: R Loreto, 57", "21967: R Loreto, 57-59", 0).
+ponto_recolha(21944, -9.14323374592279, 38.7076470601131, "R Ferragial, 33-B", "21944: R Ferragial, 33", "21944: R Ferragial, 33-B", 0).
+ponto_recolha(21932, -9.14841674214284, 38.7075210227377, "R Dom Luís I, 7-9", "21932: R Dom Luís I, 7", "21932: R Dom Luís I, 7-9", 0).
+ponto_recolha(21889, -9.14620232759384, 38.7078285138109, "Tv Carvalho, 27-27/A", "21889: Tv Carvalho, 27", "21889: Tv Carvalho, 27-27/A", 0).
+ponto_recolha(21925, -9.14259782763484, 38.7090217515467, "R António Maria Cardoso, 31/33/35", "21925: R António Maria Cardoso, 31/33/35", "21925: R António Maria Cardoso, 31/33/35", 0).
+ponto_recolha(21927, -9.14584574313843, 38.7102547130905, "Tv Sequeiro, Jt 8", "21927: Tv Sequeiro, Jt 8", "21927: Tv Sequeiro, Jt 8", 0).
+ponto_recolha(21939, -9.14408457176942, 38.7117292486178, "R Diário de Notícias, 40-B", "21939: R Diário de Notícias, 40", "21939: R Diário de Notícias, 40-B", 0).
+ponto_recolha(21949, -9.15225663484226, 38.7140002169301, "R Quintinha, 70-B", "21949: R Quintinha, 70", "21949: R Quintinha, 70-B", 0).
